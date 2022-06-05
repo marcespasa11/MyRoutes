054e89d9405959ad0441705d3178fa836563d941
Marc Espasa González <maresgon@upv.edu.es>
2022-06-05 12:23:55 +0200

    recyclerView en local (no db)

--- app/src/main/java/com/maresgon/myroutes/Fragments/FirstFragment.kt
+++ app/src/main/java/com/maresgon/myroutes/Fragments/FirstFragment.kt
@@ -6,6 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBindings
+import com.maresgon.myroutes.R
+import com.maresgon.myroutes.RecyclerAdapter
 import com.maresgon.myroutes.databinding.FragmentFirstBinding
 
 /**
@@ -14,6 +19,8 @@ import com.maresgon.myroutes.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -31,10 +38,22 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val routesRecyclerView = ViewBindings.findChildViewById<RecyclerView>(view, R.id.recyclerView)
+        routesRecyclerView.apply {
 
-        binding.buttonFirst.setOnClickListener {
-            //findNavController().navigate(R.id.action_FirstFragment_to_Second2Fragment)
+            layoutManager = LinearLayoutManager(requireContext())
+            routesRecyclerView?.layoutManager
+            adapter = RecyclerAdapter()
+            routesRecyclerView?.adapter = adapter
         }
+        /*layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        recyclerView.adapter = adapter*/
+
+        /*binding.buttonFirst.setOnClickListener {
+            //findNavController().navigate(R.id.action_FirstFragment_to_Second2Fragment)
+        }*/
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/maresgon/myroutes/RecyclerAdapter.kt
@@ -0,0 +1,50 @@
+package com.maresgon.myroutes
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    private var titles = arrayOf("Route One", "Route Two", "Route Three", "Route Four", "Route Five", "Route Six", "Route Seven", "Route Eight")
+
+    private var locations = arrayOf("loc 1", "loc 2", "loc 3", "loc 4", "loc 5", "loc 6", "loc 7", "loc 8")
+
+    private var descriptions = arrayOf("desc 1", "desc 2", "desc 3", " desc 4", "desc 5", "desc 6", "desc 7", "desc 8")
+
+    private var images = intArrayOf(R.drawable.ruta4, R.drawable.ruta4, R.drawable.ruta4, R.drawable.ruta4,
+        R.drawable.ruta4, R.drawable.ruta4, R.drawable.ruta4, R.drawable.ruta4)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        holder.itemTitle.text = titles[position]
+        holder.itemLocation.text = locations[position]
+        holder.itemDescription.text = descriptions[position]
+        holder.itemImage.setImageResource(images[position])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemLocation: TextView
+        var itemDescription: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemLocation = itemView.findViewById(R.id.item_description)
+            itemDescription = itemView.findViewById(R.id.item_description)
+        }
+    }
+}
